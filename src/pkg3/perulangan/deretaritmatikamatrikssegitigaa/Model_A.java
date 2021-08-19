d6e5b04fb3815218fc21d71cd76e023670afba6b
Rizwanda Keysha <73205628+RizwandaaK@users.noreply.github.com>
2021-08-19 13:25:20 +0700

    Add files via upload

---
+++ src/pkg3/perulangan/deretaritmatikamatrikssegitigaa/Model_A.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg3.perulangan.deretaritmatikamatrikssegitigaa;
+
+/**
+ *
+ * @author rizwa
+ */
+public class Model_A {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nilaipertama_a = 5;
+        int selisih_b = 3;
+        int jmlkolom = 8; 
+        int sukusaatini = nilaipertama_a;
+        int jmlderet = nilaipertama_a;
+        System.out.println("Deret Aritmatikanya jika dibuat segitiga sbb:");
+        System.out.println("");
+        for(int i=0;i<jmlkolom;i++){
+            for(int j=0;j<1+i;j++){
+            System.out.print(sukusaatini+"\t");
+            sukusaatini=sukusaatini+selisih_b;
+            jmlderet=jmlderet+sukusaatini;
+        }
+            System.out.println();
+        }
+        jmlderet=jmlderet-sukusaatini;
+        System.out.println("");
+        System.out.println("Jumlah Deret Aritmatikanya adalah "+jmlderet);
+    }
+}
